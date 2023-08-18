a1b22c4abb1890390804d929d595e2699a1804f1
Simanga Shabalala <simangae@gmail.com>
2023-08-18 08:13:13 +0200

    Added Category Repository

---
+++ DataAccess.InMemoryRep/CategoryRepository.cs
@@ -0,0 +1,77 @@
+﻿using B_StateOnline.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B_StateOnline.DataAccess.InMemoryRep
+{
+    public class CategoryRepository
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<Category> categories;
+        public CategoryRepository()
+        {
+            categories = cache["categories"] as List<Category>;
+            if (categories == null)
+            {
+                categories = new List<Category>();
+            }
+        }
+
+        public void Commit()
+        {
+            cache["categories"] = categories;
+        }
+
+        public void Insert(Category c)
+        {
+            categories.Add(c);
+        }
+
+        public void Update(Category c)
+        {
+            Category category = categories.Find(x => x.Id == c.Id);
+            if (category != null)
+            {
+                category = c;
+            }
+            else
+            {
+                throw new Exception("Category not found");
+            }
+        }
+        public Category Find(string Id)
+        {
+            Category category = categories.Find(c => c.Id == Id);
+            if (category != null)
+            {
+                return category;
+            }
+            else
+            {
+                throw new Exception("Category was not found");
+            }
+        }
+
+        public IQueryable<Category> Collection()
+        {
+            return categories.AsQueryable();
+        }
+
+        public void Delete(string Id)
+        {
+            Category c = categories.Find(x => x.Id == Id);
+            if (c != null)
+            {
+                categories.Remove(c);
+            }
+            else
+            {
+                throw new Exception("Category was not found");
+            }
+        }
+    }
+}
---
+++ FRONT-END/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using B_StateOnline.Core.Models;
+using B_StateOnline.DataAccess.InMemoryRep;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FRONT_END.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+       CategoryRepository context;
+        public CategoryController()
+        {
+            context = new CategoryRepository();
+        }
+        // GET: Category
+        public ActionResult Index()
+        {
+            List<Category> categories = context.Collection().ToList();
+            return View(categories);
+        }
+    }
+}
