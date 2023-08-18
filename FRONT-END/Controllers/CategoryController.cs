3d5d37efe924266551904e4eafb9e4a0df9258c4
Simanga Shabalala <simangae@gmail.com>
2023-08-18 08:48:45 +0200

    Category Controller Tested

--- FRONT-END/Controllers/CategoryController.cs
+++ FRONT-END/Controllers/CategoryController.cs
@@ -10,8 +10,7 @@ namespace FRONT_END.Controllers
 {
     public class CategoryController : Controller
     {
-
-       CategoryRepository context;
+        CategoryRepository context;
         public CategoryController()
         {
             context = new CategoryRepository();
@@ -21,6 +20,85 @@ namespace FRONT_END.Controllers
         {
             List<Category> categories = context.Collection().ToList();
             return View(categories);
+        }
+
+        public ActionResult Create()
+        {
+            Category category = new Category();
+            return View(category);
+        }
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(category);
+            }
+            else
+            {
+                context.Insert(category);
+                context.Commit();
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Edit(string Id)
+        {
+            Category category = context.Find(Id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(category);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Category category)
+        {
+            Category c = context.Find(category.Id);
+            if (c == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(category);
+                }
+                c.Name = category.Name;
+                context.Commit();
+                return RedirectToAction("Index");
+            }
+        }
+        public ActionResult Delete(string Id)
+        {
+            Category c = context.Find(Id);
+            if (c == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(c);
+            }
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult ConfirmDelete(string Id)
+        {
+            Category c = context.Find(Id);
+            if(c == null) { 
+            return HttpNotFound();
+            }
+            else
+            {
+                context.Delete(Id);
+                return RedirectToAction("index");
+            }
         }
     }
 }
