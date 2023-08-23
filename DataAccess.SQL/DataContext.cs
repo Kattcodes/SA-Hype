4ba073218bee7d3673b354c5b7bc74db4351b4c4
Katleho Makoae <Katleho Makoae@DESKTOP-J4C9BEO>
2023-08-23 23:04:46 +0200

    Added data context class with dbset.

---
+++ DataAccess.SQL/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using SA_Hype.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SA_Hype.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+            : base("DefaultConnection")
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+     }
+  } 
+
+        
+     
+  
